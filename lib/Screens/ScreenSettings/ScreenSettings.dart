a7d300aed53f201a1ba726ad8207306f8b5b0f42
saninthottungal <saninthottungalhere@gmail.com>
2024-03-12 06:57:17 +0530

    ScreenSettings UI done

--- lib/Screens/ScreenSettings/ScreenSettings.dart
+++ lib/Screens/ScreenSettings/ScreenSettings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wady/core/Constants.dart';
 
 class ScreenSettings extends StatelessWidget {
   const ScreenSettings({super.key});
@@ -6,10 +7,34 @@ class ScreenSettings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      decoration: const BoxDecoration(
+        gradient: backgroundGradient,
+      ),
       height: double.infinity,
       width: double.infinity,
-      child: const Center(
-        child: Text("Screen Settings"),
+      child: Padding(
+        padding: const EdgeInsets.only(top: 70, left: 20, right: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: Text(
+                "Dark Mode",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            Switch(
+              value: true,
+              onChanged: (value) {},
+            ),
+          ],
+        ),
       ),
     );
   }
